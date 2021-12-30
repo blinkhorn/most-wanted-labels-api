6223e111117e640f73bcf5e8bc30f744129b2bea
Patrick Blinkhorn <contact.blinkhorn@gmail.com>
2021-12-29 20:36:47 -0500

    implemented more logic for /record-labels routes

--- src/record_labels/record_labels.py
+++ src/record_labels/record_labels.py
@@ -1,43 +1,66 @@
+import os
+
+import boto3
+from boto3.dynamodb.conditions import Key
 import requests
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler.api_gateway import Router
-from environment import key, secret
+
+TABLE_NAME = os.environ["TABLE_NAME"]
+# Normally store secrets in Secrets Manager, but this is a personal AWS account, I'm
+# the only one with access, and I don't want to pay a monthly fee for Secrets
+API_KEY = os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
 
 logger = Logger(child=True)
 router = Router()
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
 
 
 @router.post("/add-new")
 def post_record_labels() -> list[dict]:
     json_payload = router.current_event.json_body
+    headers = router.current_event.headers
     record_label_name = json_payload.get('name')
+    user_id = headers.get("userId", "pblinkhorn")
     record_label_id = find_record_label_on_discogs(record_label_name)
     record_label_releases = retrieve_record_label_releases(record_label_id)
     formatted_releases_stats = format_releases_stats(record_label_releases)
     calculated_record_label_scores = calculate_record_label_scores(
         formatted_releases_stats)
-    # store_record_label_scores(calculated_record_label_scores)
-    return calculated_record_label_scores
+    return store_record_label_scores(user_id,
+                                     record_label_id, record_label_name, calculated_record_label_scores)
 
 
 @router.get("/")
 def get_record_labels() -> dict:
-    return {"items": "record_labels"}
+    headers = router.current_event.headers
+    user_id = headers.get("userId", "pblinkhorn")
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(user_id)
+    )
+    return response.get('Items')
 
 
-@router.get("/<id>")
-def get_record_label(id: str) -> dict:
-    logger.info(f"Fetching record label with id {id}")
-    return {"details": id}
+@router.get("/<record_label_id>")
+def get_record_label(record_label_id: str) -> dict:
+    logger.info(f"Fetching record label with id {record_label_id}")
+    headers = router.current_event.headers
+    user_id = headers.get("userId", "pblinkhorn")
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(user_id) & Key('record_label_id').eq(record_label_id)
+    )
+    return response.get('Items')
 
 
 def find_record_label_on_discogs(record_label_name: str) -> str:
-    url = f'https://api.discogs.com/database/search?q={record_label_name}&type=label&key={key}&secret={secret}'
+    url = f'https://api.discogs.com/database/search?q={record_label_name}&type=label&key={API_KEY}&secret={API_SECRET}'
     return requests.get(url).json().get('results')[0].get('id')
 
 
 def retrieve_record_label_releases(record_label_id: str) -> list[dict]:
-    url = f'https://api.discogs.com/labels/{record_label_id}/releases?page=1&per_page=50&sort=year&sort_order=desc&key={key}&secret={secret}'
+    url = f'https://api.discogs.com/labels/{record_label_id}/releases?page=1&per_page=50&sort=year&sort_order=desc&key={API_KEY}&secret={API_SECRET}'
     return requests.get(url).json().get('releases')
 
 
@@ -57,7 +80,7 @@ def format_releases_stats(record_label_releases: list[dict]) -> list[dict]:
 
 
 def get_release_stats(release_id: str, title: str) -> dict:
-    url = f'https://api.discogs.com/releases/{release_id}?key={key}&secret={secret}'
+    url = f'https://api.discogs.com/releases/{release_id}?key={API_KEY}&secret={API_SECRET}'
     community_response = requests.get(url).json().get('community')
     return {'have_count': community_response.get('have'), 'want_count': community_response.get('want'), 'title': title, 'id': release_id}
 
@@ -75,3 +98,21 @@ def calculate_record_label_scores(formatted_releases_stats: list[dict]) -> dict:
     # weight average_have score greater than average_want score since more people have supported label
     overall_label_score = average_have * 5 + average_want
     return {'overall_label_score': overall_label_score, 'average_want': average_want, 'average_have': average_have}
+
+
+def store_record_label_scores(user_id: str, record_label_id: str, record_label_name: str, calculated_record_label_scores: dict) -> dict:
+    formatted_result_to_store = format_stored_result(
+        user_id, record_label_id, record_label_name, calculated_record_label_scores)
+    table.put_item(Item=formatted_result_to_store)
+    return formatted_result_to_store
+
+
+def format_stored_result(user_id: str, record_label_id: str, record_label_name: str, calculated_record_label_scores: dict) -> dict:
+    return {
+        'user_id': user_id,
+        'record_label_id': record_label_id,
+        'name': record_label_name,
+        'overall_label_score': calculated_record_label_scores['overall_label_score'],
+        'average_want': calculated_record_label_scores['average_want'],
+        'average_have': calculated_record_label_scores['average_have']
+    }
